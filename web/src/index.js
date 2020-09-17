ff8e541ffb08f7ab16ea7d38bde390216ce998c9
Dave Grace <boadicea45@gmail.com>
2020-09-17 13:50:52 +1000

    menu

--- web/src/index.js
+++ web/src/index.js
@@ -5,7 +5,7 @@ import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
 import './scaffold.css'
-import './index.css'
+import './assets/main.css'
 
 ReactDOM.render(
   <FatalErrorBoundary page={FatalErrorPage}>
